87fc8d043433d7d6493714f144ee5c15c9771202
Gabriel Aldea D <gabriel.aldea@gmail.com>
2025-03-19 21:27:03 -0300

    v2

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -61,16 +61,7 @@ const Projects = () => {
       description:
         'Website created for Aldeaza Digital, a platform specializing in digital content solutions, including web design, creative media, and data-driven content management. Features custom-built tools for efficient content organization, and a sleek, responsive interface optimized with NextJS and Tailwind CSS.',
       websiteUrl: 'https://aldeazadigital.com',
-      tags: [
-        'Javascript',
-        'React',
-        'NextJS',
-        'Tailwind CSS',
-        'Document database',
-        'Image database',
-        'Excel database',
-        'Web Design',
-      ],
+      tags: ['Javascript', 'React', 'NextJS', 'Tailwind CSS', 'Web Design'],
     },
   ];
 
