1385d27d32ad58023d92e3598a18e092215148d3
Gabriel Aldea D <gabriel.aldea@gmail.com>
2025-04-01 22:32:58 -0300

    added index.ts

---
+++ src/pages/api/index.ts
@@ -0,0 +1,12 @@
+import * as express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express on Vercel');
+});
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+export default app;
